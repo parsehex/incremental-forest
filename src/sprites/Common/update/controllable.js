aecd05936e3b6fdb54ed050e3a0d0c151c3b4f46
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-05-31 03:28:47 -0400

    Move control code to common

---
+++ src/sprites/Common/update/controllable.js
@@ -0,0 +1,59 @@
+export default function(frames) {
+  checkControls.call(this, frames);
+}
+
+function checkControls(frames) {
+  const keys = this.game.state.states.Game.keys;
+
+  const upPressed = keys.UP.justPressed() || keys.W.justPressed();
+  const leftPressed = keys.LEFT.justPressed() || keys.A.justPressed();
+  const downPressed = keys.DOWN.justPressed() || keys.S.justPressed();
+  const rightPressed = keys.RIGHT.justPressed() || keys.D.justPressed();
+
+  const { tileHeight, tileWidth } = this.game.state.states.Game.map;
+
+  if (upPressed) {
+    this.frame = frames.STAND_UP;
+    tryMove.call(this, {
+      x: this.x,
+      y: this.y - tileHeight,
+    });
+  }
+  if (leftPressed) {
+    this.frame = frames.STAND_LEFT;
+    tryMove.call(this, {
+      x: this.x - tileWidth,
+      y: this.y,
+    });
+  }
+  if (downPressed) {
+    this.frame = frames.STAND_DOWN;
+    tryMove.call(this, {
+      x: this.x,
+      y: this.y + tileHeight,
+    });
+  }
+  if (rightPressed) {
+    this.frame = frames.STAND_RIGHT;
+    tryMove.call(this, {
+      x: this.x + tileWidth,
+      y: this.y,
+    });
+  }
+}
+
+function tryMove(nextCoord) {
+  const game = this.game.state.states.Game;
+
+  const nextTile = game.map.getTileWorldXY(nextCoord.x, nextCoord.y, 32, 32, 'foreground');
+
+  if (nextTile !== null) {
+    tweenMove.call(this, nextCoord);
+  }
+}
+
+function tweenMove(nextCoord) {
+  const move = this.game.add.tween(this);
+
+  move.to(nextCoord, 50, null, true);
+}
--- src/sprites/Player/update.js
+++ src/sprites/Player/update.js
@@ -1,61 +1,6 @@
 import frames from '../../sprite-frames';
+import controllable from '../Common/update/controllable';
 
 export default function update() {
-  checkControls.call(this);
-}
-
-function checkControls() {
-  const keys = this.game.state.states.Game.keys;
-
-  const upPressed = keys.UP.justPressed();
-  const leftPressed = keys.LEFT.justPressed();
-  const downPressed = keys.DOWN.justPressed();
-  const rightPressed = keys.RIGHT.justPressed();
-
-  const { tileHeight, tileWidth } = this.game.state.states.Game.map;
-
-  if (upPressed) {
-    this.frame = frames.GUY.STAND_UP;
-    tryMove.call(this, {
-      x: this.x,
-      y: this.y - tileHeight,
-    });
-  }
-  if (leftPressed) {
-    this.frame = frames.GUY.STAND_LEFT;
-    tryMove.call(this, {
-      x: this.x - tileWidth,
-      y: this.y,
-    });
-  }
-  if (downPressed) {
-    this.frame = frames.GUY.STAND_DOWN;
-    tryMove.call(this, {
-      x: this.x,
-      y: this.y + tileHeight,
-    });
-  }
-  if (rightPressed) {
-    this.frame = frames.GUY.STAND_RIGHT;
-    tryMove.call(this, {
-      x: this.x + tileWidth,
-      y: this.y,
-    });
-  }
-}
-
-function tryMove(nextCoord) {
-  const game = this.game.state.states.Game;
-
-  const nextTile = game.map.getTileWorldXY(nextCoord.x, nextCoord.y, 32, 32, 'foreground');
-
-  if (nextTile !== null) {
-    tweenMove.call(this, nextCoord);
-  }
-}
-
-function tweenMove(nextCoord) {
-  const move = this.game.add.tween(this);
-
-  move.to(nextCoord, 50, null, true);
+  controllable.call(this, frames.GUY);
 }
