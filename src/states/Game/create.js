9daaa82fe2625c93c495364fc60c6cca4d9df595
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-05-31 03:28:29 -0400

    Add WASD

--- src/states/Game/create.js
+++ src/states/Game/create.js
@@ -47,10 +47,14 @@ function setupKeys() {
   const keyboard = this.game.input.keyboard;
 
   this.keys = {
-    LEFT: keyboard.addKey(Phaser.Keyboard.LEFT),
-    RIGHT: keyboard.addKey(Phaser.Keyboard.RIGHT),
     UP: keyboard.addKey(Phaser.Keyboard.UP),
+    LEFT: keyboard.addKey(Phaser.Keyboard.LEFT),
     DOWN: keyboard.addKey(Phaser.Keyboard.DOWN),
+    RIGHT: keyboard.addKey(Phaser.Keyboard.RIGHT),
+    W: keyboard.addKey(Phaser.Keyboard.W),
+    A: keyboard.addKey(Phaser.Keyboard.A),
+    S: keyboard.addKey(Phaser.Keyboard.S),
+    D: keyboard.addKey(Phaser.Keyboard.D),
     SPACE: keyboard.addKey(Phaser.Keyboard.SPACEBAR),
   };
 
