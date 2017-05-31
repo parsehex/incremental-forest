f626e3adc768ce69c283b4a137a9ade73e582663
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-05-31 03:15:58 -0400

    Add moving

--- src/sprites/Player/update.js
+++ src/sprites/Player/update.js
@@ -10,34 +10,46 @@ function checkControls() {
   const downPressed = keys.DOWN.justPressed();
   const rightPressed = keys.RIGHT.justPressed();
 
-  const tileSize = 32;
+  const { tileHeight, tileWidth } = this.game.state.states.Game.map;
 
   if (upPressed) {
     tryMove.call(this, {
       x: this.x,
-      y: this.y - tileSize,
+      y: this.y - tileHeight,
     });
   }
   if (leftPressed) {
     tryMove.call(this, {
-      x: this.x - tileSize,
+      x: this.x - tileWidth,
       y: this.y,
     });
   }
   if (downPressed) {
     tryMove.call(this, {
       x: this.x,
-      y: this.y + tileSize,
+      y: this.y + tileHeight,
     });
   }
   if (rightPressed) {
     tryMove.call(this, {
-      x: this.x + tileSize,
+      x: this.x + tileWidth,
       y: this.y,
     });
   }
 }
 
 function tryMove(nextCoord) {
-  //
+  const game = this.game.state.states.Game;
+
+  const nextTile = game.map.getTileWorldXY(nextCoord.x, nextCoord.y, 32, 32, 'foreground');
+
+  if (nextTile !== null) {
+    tweenMove.call(this, nextCoord);
+  }
+}
+
+function tweenMove(nextCoord) {
+  const move = this.game.add.tween(this);
+
+  move.to(nextCoord, 50, null, true);
 }
