13e6d355f8d3372016ed52a98dd5969d46a6bbf4
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-02 12:42:30 -0400

    Load logs sprite

--- src/states/Splash/preload.js
+++ src/states/Splash/preload.js
@@ -14,6 +14,8 @@ export default function preload() {
 
   this.game.load.tilemap('forest', '/assets/tilemaps/forest.json', null, Phaser.Tilemap.TILED_JSON);
 
+  this.game.load.spritesheet('logs', '/assets/sprites/other/logs.png', 32, 32);
+
   this.game.load.spritesheet('tiles_ground', '/assets/sprites/ground.png', 32, 32);
   this.game.load.spritesheet('tiles_generator', '/assets/sprites/other/generator.png', 32, 32);
 }
