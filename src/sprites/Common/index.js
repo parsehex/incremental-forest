0ed42a900e206c2cd78fdf6b5f6578254394f3e2
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-03 13:15:23 -0400

    Align to grid before placing object

--- src/sprites/Common/index.js
+++ src/sprites/Common/index.js
@@ -1,7 +1,14 @@
 import Phaser from 'phaser';
 
+import { alignToGrid } from '../../tiles';
+
 export default class extends Phaser.Sprite {
   constructor(game, x, y, sprite, frame) {
+    const alignedCoords = alignToGrid({ x, y });
+
+    x = alignedCoords.x;
+    y = alignedCoords.y;
+
     super(game, x, y, sprite, frame);
 
     this.anchor.setTo(0.5, 0.5);
