ce342908f3f959379c4c6fc5a870d16f2c66df0d
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-06 12:56:24 -0400

    Grow tree from placed seeds

--- src/sprites/PineCone/placed.js
+++ src/sprites/PineCone/placed.js
@@ -1,5 +1,22 @@
+import Phaser from 'phaser';
+import Tree from '../Tree';
+
+const chance = 15;
+
 export default function placed() {
-  // start recursive timer
-  // gen random number
-  // decide if grow tree
+  this.game.time.events.add(Phaser.Timer.SECOND * 5, function() {
+    const number = Math.floor(Math.random() * 100) + 1;
+
+    if (chance <= number) {
+      new Tree({
+        game: this.game,
+        x: this.x,
+        y: this.y,
+      });
+
+      this.destroy();
+    } else {
+      placed.call(this);
+    }
+  }, this);
 }
