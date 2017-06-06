da8c492ff3bf50ae4d49d95ca5858600752b8b43
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-06 10:20:31 -0400

    Fix controls not holding properly

--- src/sprites/Player/update/watch-controls.js
+++ src/sprites/Player/update/watch-controls.js
@@ -29,32 +29,29 @@ export default function(moveCallback, interactCallback) {
 
 function checkKey(callback, key) {
   const keys = this.game.state.states.Game.keys;
+  const directionKey = directionToWASD(key);
 
-  let pressed;
-  if (key === 'SPACE') {
-    pressed = keys[key].justPressed();
-  } else {
-    const directionKey = directionToWASD(key);
-    pressed = keys[key].justPressed() || keys[directionKey].justPressed();
+  const adjKey = directionKey || key;
+
+  let pressed = keys[adjKey].justPressed();
+
+  if (adjKey !== 'SPACE') {
+    pressed = pressed || keys[directionKey].justPressed();
   }
-
 
   if (pressed) {
     callback(key);
 
-    if (this.timers[key]) return;
-
-    this.timers[key] = this.game.time.events.loop(Phaser.Timer.SECOND / 5, function() {
-      if (
-        !keys[key].isDown ||
-        (typeof directionKey !== 'undefined' && keys[directionKey].justPressed())
-      ) {
-        this.timers[key].timer.destroy();
-        this.timers[key] = null;
-        return;
-      }
-
-      callback(key);
-    }, this);
+    loop.call(this, keys, adjKey, callback, key);
   }
+}
+
+function loop(keys, name, callback, callbackArg) {
+  this.game.time.events.add(Phaser.Timer.SECOND / 5, function() {
+    if (!keys[name].isDown) return;
+
+    callback(callbackArg);
+
+    loop.call(this, keys, name, callback, callbackArg);
+  }, this);
 }
