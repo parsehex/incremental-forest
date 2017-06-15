0f15682a9888b50cab743e0656228e463f005346
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-15 12:24:06 -0400

    Add building sprite

--- build-spritesheets/index.js
+++ build-spritesheets/index.js
@@ -25,11 +25,11 @@ function processArgs() {
 
 // loop through each spritesheet specified in json
 for (let i = 0; i < spritesheets.length; i++) {
-  const { name, sprites } = spritesheets[i];
+  const { name, size, sprites } = spritesheets[i];
 
   // spritesheet should as wide as (number of sprites * 32px)
-  const width  = sprites.length * 32;
-  const height = 32;
+  const width  = sprites.length * size;
+  const height = size;
 
   // create a new image for this spritesheet
   new Jimp(width, height, function (err, spritesheet) {
@@ -40,7 +40,7 @@ for (let i = 0; i < spritesheets.length; i++) {
       // read sprite at specified file, call concat once read to composite into spritesheet
       let spritePath = path.join(__dirname, pathPrefix, sprites[i]);
       promises.push(
-        Jimp.read(spritePath).then(concat.bind(null, i, spritesheet))
+        Jimp.read(spritePath).then(concat.bind({ size }, i, spritesheet))
       );
     }
 
@@ -53,5 +53,5 @@ for (let i = 0; i < spritesheets.length; i++) {
 
 function concat(i, spritesheet, sprite) {
   // composite (concat) the sprite to the spritesheet
-  spritesheet.composite(sprite, i * 32, 0);
+  spritesheet.composite(sprite, i * this.size, 0);
 }
