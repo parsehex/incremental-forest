b6306e0e619a40b25cdc7b8377ed4c952e8cc0a7
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-15 14:50:11 -0400

    Add notify function

--- src/ui/notifications/index.js
+++ src/ui/notifications/index.js
@@ -1,3 +1,25 @@
 export function notify(type, text) {
-  // type decides color
+  const notificationsEl = document.querySelector('#notifications ul');
+
+  const notificationLi = document.createElement('li');
+  notificationLi.className = type;
+
+  const dismissDiv = document.createElement('div');
+  dismissDiv.className = 'dismiss';
+  dismissDiv.textContent = 'x';
+  dismissDiv.addEventListener('click', dismiss.bind(notificationLi));
+  notificationLi.appendChild(dismissDiv);
+
+  const textSpan = document.createElement('span');
+  textSpan.textContent = text;
+  notificationLi.appendChild(textSpan);
+
+  notificationsEl.appendChild(notificationLi);
+}
+
+function dismiss(event) {
+  this.addEventListener('transitionend', () => {
+    this.remove();
+  });
+  this.style.opacity = 0;
 }
