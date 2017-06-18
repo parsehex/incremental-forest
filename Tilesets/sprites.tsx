2470228a30dd9e7f57c8482f48ed5e2bb57dd3e7
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-18 18:07:39 -0400

    Fix sprite path

--- Tilesets/sprites.tsx
+++ Tilesets/sprites.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="sprites" tilewidth="32" tileheight="32" tilecount="6" columns="6">
- <image source="../assets/sprites/sprites.png" width="192" height="32"/>
+<tileset name="sprites" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="../assets/spritesheets/tiles.png" width="256" height="32"/>
 </tileset>
