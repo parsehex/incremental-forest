456ff7adbccf88ee8b9b99f84b0a3f806ea37c08
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-21 03:31:37 -0400

    Add findWork script

--- src/sprites/character/Worker/find-work.js
+++ src/sprites/character/Worker/find-work.js
@@ -3,34 +3,23 @@ import { clone, nextCoord } from '../../../utils';
 import { collidableObjects } from '../../../collisions';
 import config from '../../../config';
 
-const directions = [ 'UP', 'DOWN', 'LEFT', 'RIGHT'];
-
-function convertPath(path) {
-  for (let i = 0; i < path.length; i++) {
-    path[i] = directions[path[i]];
-  }
-  return path;
-}
+import workerPool from '../../../worker-pool';
 
 export default function findWork() {
   if (this.working || this.waitingOnPath) return;
 
   const collidables = collidableObjects.concat(['chopper', 'collector', 'planter']);
 
-  const worker = this.pathFindWorker;
   const workerArgs = [
     fastMap, fastObjects, collidables,
     config.mapWidth, config.mapHeight,
-    this.tile, 'tree', // start, target
+    this.tile, this.targetObjects, // use this.targetObjects
   ];
 
   this.waitingOnPath = true;
-  worker.postMessage(workerArgs);
-  worker.onmessage = (event) => {
-    const pathToTree = event.data;
-
+  workerPool.addTask(workerArgs, (pathToTree) => {
     if (pathToTree) {
-      this.path = convertPath(pathToTree);
+      this.path = pathToTree;
 
       this.noPath = false;
       this.working = true;
@@ -39,7 +28,7 @@ export default function findWork() {
       this.cancelWork(true);
     }
     this.waitingOnPath = false;
-  };
+  })
 
   // if a path to a tree can't be found, worker will try again next time update calls it
 }
