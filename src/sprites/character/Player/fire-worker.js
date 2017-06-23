1baeedc420080b3604f99db92cf1a8aa8aa31219
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-22 23:38:53 -0400

    Fire closest worker of correct type
    Also fixes worker count going into negative when firing without having
    any employed workers

--- src/sprites/character/Player/fire-worker.js
+++ src/sprites/character/Player/fire-worker.js
@@ -1,11 +1,27 @@
-import { findObjByKey } from '../../../utils';
-
 export default function fireWorker(workerType) {
-  const characters = this.game.state.states.Game.groups.character.children;
+  const worker = findNearest(this.game.state.states.Game.groups.character.children, this.tile, workerType);
 
-  const worker = findObjByKey(characters, 'objectType', workerType);
+  if (!worker) return;
 
-  if (worker) {
-    worker.destroy();
+  worker.destroy();
+}
+
+export function findNearest(workers, tileCoord, workerType) {
+  let closestDistance = null;
+  let closestWorker = null;
+
+  for (let i = 0; i < workers.length; i++) {
+    const worker = workers[i];
+    if (worker.objectType !== workerType || !worker.alive) continue;
+
+    const yDist = Math.abs(tileCoord.y - worker.tile.y);
+    const xDist = Math.abs(tileCoord.x - worker.tile.x);
+
+    if (closestDistance > (yDist + xDist) || closestDistance === null) {
+      closestDistance = yDist + xDist;
+      closestWorker = worker;
+    }
   }
+
+  return closestWorker;
 }
