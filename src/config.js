4c704a01973508952b781314cac595af38e65cf2
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-25 07:28:59 -0400

    Update main level

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import devtools from './devtools';
 export default {
   gameWidth: 500,
   gameHeight: 400,
-  mapWidth: devtools.enabled && devtools.testMap ? devtools.testMapSize[0] : 25,
-  mapHeight: devtools.enabled && devtools.testMap ? devtools.testMapSize[1] : 25,
+  mapWidth: devtools.enabled && devtools.testMap ? devtools.testMapSize[0] : 30,
+  mapHeight: devtools.enabled && devtools.testMap ? devtools.testMapSize[1] : 30,
   tileWidth: 32,
   tileHeight: 32,
 };
