852c827da8470f96033abf80aa4c107e0abf7531
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-26 11:47:06 -0400

    Add pine cone drop and tree grow chance upgrades

--- src/ui/setup/menus/upgrades.js
+++ src/ui/setup/menus/upgrades.js
@@ -1,4 +1,5 @@
 import bindMenu from '../bind-menu';
+import { increaseChance } from '../../../game-data/chances';
 
 export default function setup() {
   bindMenu('upgrades');
@@ -13,6 +14,34 @@ export default function setup() {
 
     player.inventory.money.value -= price;
     player.inventory.items['wood-axe'].rank++;
+  });
+
+  upgrade('pine-cone', (event) => {
+    const price = +event.target.dataset.price;
+    const upgradeNumber = +event.target.dataset.upgradeNumber || 0;
+    const player = this.game.state.states.Game.player;
+
+    if (player.inventory.money.value < price || upgradeNumber >= 23) return;
+
+    updatePrice('pine-cone', 5 * Math.pow(2, upgradeNumber + 1));
+
+    player.inventory.money.value -= price;
+    increaseChance('seedDrop');
+    event.target.dataset.upgradeNumber = upgradeNumber + 1;
+  });
+
+  upgrade('tree-grow', (event) => {
+    const price = +event.target.dataset.price;
+    const upgradeNumber = +event.target.dataset.upgradeNumber || 0;
+    const player = this.game.state.states.Game.player;
+
+    if (player.inventory.money.value < price || upgradeNumber >= 23) return;
+
+    updatePrice('tree-grow', 15 * Math.pow(2.1, upgradeNumber + 1));
+
+    player.inventory.money.value -= price;
+    increaseChance('treeGrow');
+    event.target.dataset.upgradeNumber = upgradeNumber + 1;
   });
 }
 
