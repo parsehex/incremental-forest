1c1dcf8c6d916a55549b66564e73b07fa76bd63a
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-06-29 11:11:57 -0400

    Update build

--- web-worker/path-find.js
+++ web-worker/path-find.js
@@ -1,25 +1,68 @@
+const directions = ['UP', 'DOWN', 'LEFT', 'RIGHT'];
+function convertPath(path) {
+  for (let i = 0; i < path.length; i++) {
+    path[i] = directions[path[i]];
+  }
+  return path;
+}
+
+let firstRun = true;
 onmessage = function(event) {
   // expect posted message data to be in a certain format
   if (!Array.isArray(event.data)) throw new Error('pass an array to worker');
 
-  postMessage(findPath(event.data));
+  let timing = false;
+
+  let now;
+  if (timing) now = performance.now();
+
+  let path = findPath(event.data);
+  if (path) path.path = convertPath(path.path);
+
+  postMessage(path);
+
+  if (timing) console.log(performance.now() - now);
+}
+
+function shuffle(array) {
+  // https://stackoverflow.com/a/2450976
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 function findPath(data) {
-  // fastMap, fastObjects expected
-  let fastMap =           data[0],
-      fastObjects =       data[1],
-      collidableObjects = data[2];
+  const fastMap =           data[0],
+        fastObjects =       data[1],
+        collidableObjects = data[2];
 
-  let mapWidth =          data[3],
-      mapHeight =         data[4];
+  const mapWidth =          data[3],
+        mapHeight =         data[4];
 
   let queue =             [],
       checkedTiles =      [],
       start =             data[5],
-      target =            data[6];
+      target =            data[6],
+      random =            data[7];
 
-  if (typeof target === 'string' && !fastObjects.includes(target)) return;
+  if (typeof target === 'string') {
+    target = [target];
+  }
+
+  const targetIsArray = Array.isArray(target);
 
   const startLocation = {
     x: start.x,
@@ -32,29 +75,19 @@ function findPath(data) {
 
   checkedTiles.push(startLocation.x + ',' + startLocation.y);
 
-  let debug = 0;
-  while (queue.length && debug <= 1000) { // infinite loop protection
+  while (queue.length) {
     let item = queue[0];
 
-    const up = processDirection(item, 'UP');
-    if (up) return up;
+    const directions = random ? shuffle([0, 2, 1, 3]) : [0, 1, 2, 3];
 
-    const left = processDirection(item, 'LEFT');
-    if (left) return left;
-
-    const down = processDirection(item, 'DOWN');
-    if (down) return down;
-
-    const right = processDirection(item, 'RIGHT');
-    if (right) return right;
+    for (let i = 0; i < directions.length; i++) {
+      const direction = processDirection(item, directions[i]);
+      if (direction) return direction;
+    }
 
     // remove first item in queue
     queue.shift();
-
-    debug++;
   }
-
-  return [];
 
   function processDirection(location, direction) {
     const result = checkDirection(location, direction);
@@ -71,25 +104,37 @@ function findPath(data) {
     if (result.outOfBounds) return;
 
     let isTarget = false;
-    if (typeof target === 'string') {
-      isTarget = checkTargetType(coordName, target);
+    if (targetIsArray) {
+      isTarget = checkTargetType(result, target);
+    } else if (target === null) {
+      isTarget = checkTargetEmpty(result);
     } else {
       isTarget = checkTargetLocation(result);
     }
 
     if (isTarget) {
       // result is the target
-      return result.path;
+      return result;
     } else if (result.walkable) {
       // result isn't target but is still walkable; add to queue to check
       queue.push(result);
     }
   }
 
-  function checkTargetType(coordName, targetType) {
-    // TODO prefer (but not require) tiles with no objects at all on them
+  function checkTargetType(location, targetTypes) {
+    const mapTile = fastMap[location.y][location.x];
 
-    return fastMap[coordName].includes(targetType);
+    if (mapTile.length === 0) return false;
+
+    for (let i = 0; i < targetTypes.length; i++) {
+      if (mapTile.includes(targetTypes[i])) {
+        return true;
+      }
+    }
+  }
+
+  function checkTargetEmpty(location) {
+    return fastMap[location.y][location.x].length === 0;
   }
 
   function checkTargetLocation(location) {
@@ -99,10 +144,11 @@ function findPath(data) {
   }
 
   function checkDirection(location, direction) {
+    // manually clone location object
     location = {
       x: location.x,
       y: location.y,
-      path: JSON.parse(JSON.stringify(location.path)),
+      path: location.path.slice(), // clones location.path (quicker than JSON.parse+stringify)
       walkable: location.walkable,
     };
 
@@ -111,14 +157,15 @@ function findPath(data) {
     location.walkable = true;
 
     // update location coords based on direction
-    nextCoord(location, direction, 1);
+    nextCoord(location, direction);
+    const { x, y } = location;
 
-    if (location.x < 0 || location.x >= mapWidth || location.y < 0 || location.y >= mapHeight) {
+    if (x < 0 || x >= mapWidth || y < 0 || y >= mapHeight) {
       location.outOfBounds = true;
       return location;
     }
 
-    const mapTile = fastMap[location.x + ',' + location.y];
+    const mapTile = fastMap[y][x];
 
     for (let i = 0; i < collidableObjects.length; i++) {
       if (mapTile.includes(collidableObjects[i])) {
@@ -132,26 +179,14 @@ function findPath(data) {
 }
 
 // utility function dependencies
-const clone = (obj) => JSON.parse(JSON.stringify(obj));
-function nextCoord(coord, direction, size) {
-  const { x, y } = coord;
-
-  switch (direction) {
-    case 'UP': {
-      coord.y = y - size;
-      break;
-    }
-    case 'LEFT': {
-      coord.x = x - size;
-      break;
-    }
-    case 'DOWN': {
-      coord.y = y + size;
-      break;
-    }
-    case 'RIGHT': {
-      coord.x = x + size;
-      break;
-    }
+function nextCoord(coord, direction) {
+  if (direction === 0) {
+    coord.y = coord.y - 1;
+  } else if (direction === 1) {
+    coord.y = coord.y + 1;
+  } else if (direction === 2) {
+    coord.x = coord.x - 1;
+  } else if (direction === 3) {
+    coord.x = coord.x + 1;
   }
 }
