5fe8fcda8d2a1d8cdc5ff4a61fa8582e875df3f3
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-07-11 14:28:49 -0400

    Refactor, organize utility functions

---
+++ src/utils/geometry.js
@@ -0,0 +1,40 @@
+import config from '../config';
+
+export function centerOfObject(object, map) {
+  return {
+    x: object.x + (map.tileWidth / 2),
+    y: object.y - (map.tileHeight / 2),
+  };
+}
+
+export function nextCoord(coord, direction, size) {
+  const { x, y } = coord;
+
+  switch (direction) {
+    case 'UP': {
+      return { x: x, y: y - size };
+    }
+    case 'LEFT': {
+      return { x: x - size, y: y };
+    }
+    case 'DOWN': {
+      return { x: x, y: y + size };
+    }
+    case 'RIGHT': {
+      return { x: x + size, y: y };
+    }
+  }
+}
+
+export function pixelOutOfBounds(pixelCoord) {
+  return (
+    pixelCoord.x < 0 || pixelCoord.x >= config.gameWidth ||
+    pixelCoord.y < 0 || pixelCoord.y >= config.gameHeight
+  );
+}
+export function tileOutOfBounds(tileCoord) {
+  return (
+    tileCoord.x < 0 || tileCoord.x >= config.mapWidth ||
+    tileCoord.y < 0 || tileCoord.y >= config.mapHeight
+  );
+}
