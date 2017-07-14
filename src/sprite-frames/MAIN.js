1b4634e3abef6bc9ad5449eaa6f43921587f54d5
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2017-07-13 21:35:17 -0400

    Remove generator from spritesheet

--- src/sprite-frames/MAIN.js
+++ src/sprite-frames/MAIN.js
@@ -5,9 +5,7 @@ export default {
   MUD:           1,
   LOG:           2,
   PINE_CONE:     3,
-  GENERATOR_OFF: 4,
-  GENERATOR_ON:  5,
-  TREE:          6,
-  WATER:         7,
-  DIRT:          8,
+  TREE:          4,
+  WATER:         5,
+  DIRT:          6,
 };
